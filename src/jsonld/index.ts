92dac9505225f832a24dc7d6beda4350884074d4
Cristiano Santos <cristianorsantos.95@gmail.com>
2026-05-07 09:58:54 +0100

    feat(jsonld): add compose() and public exports

--- src/jsonld/index.ts
+++ src/jsonld/index.ts
@@ -1,1 +1,16 @@
-export {};
+export { apiReference, type ApiReferenceInput } from "./api-reference.js";
+export { article, type ArticleInput } from "./article.js";
+export { breadcrumbList, type Breadcrumb } from "./breadcrumb.js";
+export { compose } from "./compose.js";
+export {
+  definedTerm,
+  definedTermSet,
+  type DefinedTermInput,
+  type DefinedTermSetInput,
+} from "./defined-term.js";
+export { faqPage, type FaqItem } from "./faq.js";
+export { organization, type OrganizationInput } from "./organization.js";
+export { person, type PersonInput } from "./person.js";
+export { speakable, type SpeakableInput } from "./speakable.js";
+export { schemaId } from "./types.js";
+export { website, type WebSiteInput } from "./website.js";
